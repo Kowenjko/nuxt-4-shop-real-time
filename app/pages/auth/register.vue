7b5755bc36443f7859dfa067a4d72ffe6a328ef8
Kowenjko <ortoswt@gmail.com>
2025-10-21 19:20:10 +0300

    Create project

---
+++ app/pages/auth/register.vue
@@ -0,0 +1,8 @@
+<script lang="ts" setup>
+definePageMeta({
+	layout: 'auth',
+})
+</script>
+
+<template><div class="bg-slate-200 h-screen">Register</div></template>
+v
---
+++ app/pages/index.vue
@@ -0,0 +1,5 @@
+<script lang="ts" setup></script>
+
+<template>
+	<main><h1>Index</h1></main>
+</template>
