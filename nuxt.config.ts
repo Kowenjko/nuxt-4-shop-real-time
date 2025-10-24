bf0cf099299b04197adbb0006d7f389426fe8519
Kowenjko <ortoswt@gmail.com>
2025-10-24 10:20:51 +0300

    install prisma

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,12 @@
 export default defineNuxtConfig({
 	compatibilityDate: '2025-07-15',
 	devtools: { enabled: true },
-	modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt', 'nuxt-typed-router'],
+	modules: [
+		'@nuxtjs/tailwindcss',
+		'@pinia/nuxt',
+		'nuxt-typed-router',
+		'@nuxt/image',
+	],
 
 	components: [
 		{
@@ -12,5 +17,14 @@ export default defineNuxtConfig({
 	],
 	pinia: {
 		storesDirs: ['./stores/**'],
+	},
+
+	runtimeConfig: {
+		//private: is accessible only on the server
+		JWT_TOKEN_KEY: process.env.JWT_TOKEN_KEY,
+		REFRESH_TOKEN_KEY: process.env.REFRESH_TOKEN_KEY,
+		DATABASE_URL: process.env.DATABASE_URL,
+		DIRECT_URL: process.env.DIRECT_URL,
+		public: {},
 	},
 })
---
+++ server/api/users.ts
@@ -0,0 +1,6 @@
+// server/api/users.ts
+// import prisma from '../../lib/prisma'
+
+export default defineEventHandler(async () => {
+	// return prisma.user.findMany()
+})
